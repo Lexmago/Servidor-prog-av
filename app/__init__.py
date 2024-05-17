cb78bf8dcde7baae00e713755cf129b752387582
Lexmago <stefanwork66@gmail.com>
2024-05-17 15:57:24 -0600

    lanzamiento 1.0.1

---
+++ app/__init__.py
@@ -0,0 +1,14 @@
+# Importamos la librería Flask
+from flask import Flask
+from flask_restful import Api
+from .routes import APIRoutes
+
+# Creamos una función para montar el servidor
+def crear_app():
+    app = Flask(__name__)
+    api = Api(app)
+
+    routes = APIRoutes()
+    routes.init_routes(api)
+    # Regresamos ese servidor montado
+    return app
