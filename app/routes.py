1a71332fd84a8afef99ac63a0d739d85d074ecf7
Lexmago <stefanwork66@gmail.com>
2024-05-25 17:15:23 -0600

    actualizacion 1.0.0

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,36 @@
 from flask_restful import Resource
 
+lista_objetos_almacen = [
+{
+    'id': 1,
+    'nombre': 'Lapiz',
+    'cantidad': 4
+},
+{
+    'id': 2,
+    'nombre': 'Goma',
+    'cantidad': 3  
+},
+{
+    'id': 3,
+    'nombre': 'Tijeras',
+    'cantidad': 6
+}
+]
+
 class HelloWorld(Resource):
     def get(self):
         return {'message': 'Hola mundo desde la API', 'status': 200}
     
+class Almacen(Resource):
+    # Obtenemos la informacion del almacen
+    def get(self):
+        return {'Objetos': lista_objetos_almacen, 'status': 200}
+    # Ponemos un nuevo objeto en el almacen
+    def post(self):
+        return {'received': True, 'status': 200}
+    
 class APIRoutes():
     def init_routes(self, api):
         api.add_resource(HelloWorld, '/')
+        api.add_resource(Almacen, '/objetos_almacen')
