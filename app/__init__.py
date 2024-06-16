138a42b8e487e008aa29ab50272a16fd3f7cc675
Lexmago <stefanwork66@gmail.com>
2024-06-16 10:22:16 -0600

    Configurando todo para DB

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,14 @@
 from flask import Flask
 from flask_restful import Api
 from .routes import APIRoutes
+from .config import Config
+from .extensions import db
 
 # Creamos una función para montar el servidor
 def crear_app():
     app = Flask(__name__)
+    db.init_app(app) #Aqui la DB se va a inicializar en nuestra aplicación
+    app.config.from_object(Config) #Nuestra app se va a configurar desde un objeto
     api = Api(app)
 
     routes = APIRoutes()
