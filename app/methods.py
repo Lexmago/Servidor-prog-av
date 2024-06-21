a6402577efe1ac7f8444acad336151136cd5c3a8
Lexmago <stefanwork66@gmail.com>
2024-06-20 18:53:36 -0600

    Creando Nuevos Productos en la DB

--- app/methods.py
+++ app/methods.py
@@ -1,17 +1,42 @@
+from app.extensions import db
+from .models.producto import Producto
 
-def buscar_elemento_id_nombre(lista_objetos_almacen,parametro_id, parametro_nombre):
+def buscar_elemento_id_nombre(parametro_id, parametro_nombre):
     if parametro_id != None:
-        for objeto in lista_objetos_almacen:
-            if objeto.get('id') == int(parametro_id):
-                return { 'objeto': objeto, 'status': 200}
-                
-        return {'Mensaje': 'Objeto no encontrado', 'status': 404}
-        
+
+        producto_obtenido = Producto.query.get_or_404(parametro_id)
+
+        json_retornado = {
+            'ID': producto_obtenido.id,
+            'Nombre': producto_obtenido.nombre,
+            'Cantidad': producto_obtenido.cantidad
+        }
+    
+        return json_retornado
+    
     elif parametro_nombre != None:
-        for objeto in lista_objetos_almacen:
-            if objeto.get('nombre') == parametro_nombre.capitalize():
-                return { 'objeto': objeto, 'status': 200}
-                
-        return {'Mensaje': 'Objeto no encontrado', 'status': 404}
-        
-    return {'Objetos': lista_objetos_almacen, 'status': 200}
+        producto_obtenido = Producto.query.filter_by(nombre=parametro_nombre).first_or_404()
+
+        json_retornado = {
+            'ID': producto_obtenido.id,
+            'Nombre': producto_obtenido.nombre,
+            'Cantidad': producto_obtenido.cantidad
+        }
+
+        return json_retornado
+    
+    else:
+        return {'Error': 'No pusiste ninguna Query'}, 500 # 500 de internal server error
+    
+def crear_producto(nombre, cantidad):
+    nuevo_producto = Producto(nombre=nombre, cantidad=cantidad)
+    db.session.add(nuevo_producto)
+    db.session.commit()
+    
+    json_retornado = {
+            'ID': nuevo_producto.id,
+            'Nombre': nuevo_producto.nombre,
+            'Cantidad': nuevo_producto.cantidad
+    }
+    
+    return json_retornado
--- app/routes.py
+++ app/routes.py
@@ -4,23 +4,6 @@ from flask import request
 #Importamos los metodos de nuestra API
 from .methods import *
 
-lista_objetos_almacen = [
-{
-    'id': 1,
-    'nombre': 'Lapiz',
-    'cantidad': 4
-},
-{
-    'id': 2,
-    'nombre': 'Goma',
-    'cantidad': 3  
-},
-{
-    'id': 3,
-    'nombre': 'Tijeras',
-    'cantidad': 6
-}
-]
 
 class HelloWorld(Resource):
     def get(self):
@@ -33,15 +16,13 @@ class Almacen(Resource):
         parametro_id = request.args.get('id')
         parametro_nombre = request.args.get('nombre')
 
-        return buscar_elemento_id_nombre(lista_objetos_almacen, parametro_id, parametro_nombre)
+        return buscar_elemento_id_nombre(parametro_id, parametro_nombre)
 
     def post(self):
         # Se crea una nueva variable para guardar la información que posteó el usuario
         data = request.get_json()
-        # Agregamos la información a la lista del almacén
-        lista_objetos_almacen.append(data)
 
-        return {'received': True, 'info': data, 'status': 200}
+        return crear_producto(data['nombre'], data['cantidad'])
     
 class APIRoutes():
     def init_routes(self, api):
